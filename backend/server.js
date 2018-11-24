2ac5d702b40535e470ec81eb0106a685e13c41cd
K <codehaag@gmail.com>
2018-11-23 21:34:12 -0800

    Added POST to server.js

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,26 @@ server.get('/', (req, res) => {
         .send(data)
 })
 
-
 server.get('/:id', (req, res) => {
     const data = DB.filter(data => (data.id === parseInt(req.params.id)))
+    if(data.length > 1) return res.status(500).send({msg: "Something went terribly wrong on the server's end"})
+    if(data.length === 0) return res.status(404).send({msg: "That location doesn't seem to exist"})
+    if(data.length < 1) return res.status(500).send({msg: "Something went terribly wrong on the server's end"})
     res.send(data[0])
+})
+
+
+server.post('/', (req, res) => {
+    const {name, notes} = req.body
+    const newPerson = {
+        id: DB.length + 1,
+        name,
+        notes,
+    }
+    DB.push(newPerson)
+    res.status(201)
+    res.send(newPerson)
+
 })
 
 
