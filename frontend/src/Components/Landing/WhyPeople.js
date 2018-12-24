2eafd5b91abfb3c8a3b642e0db8771afd97e9e0d
K <codehaag@gmail.com>
2018-12-24 12:43:09 -0800

    Working on Landing

--- frontend/src/Components/Landing/WhyPeople.js
+++ frontend/src/Components/Landing/WhyPeople.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const WhyPeople = () => {
     return (
-        <WhyPeople id="why">
-            <div>
+        <Why id="why">
+            <h2>
                 This is why you should use People....
-            </div>
-        </WhyPeople>
+            </h2>
+        </Why>
     )
 }
 
 
 export default WhyPeople
+
+
+const Why = styled.div`
+background-color: yellow;
+padding-top: 55px;
+border: 1px solid pink;
+height: 100vh;
+width: 100%;
+`;
